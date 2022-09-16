7dcd7493327c7bd41ac9428b144927442fc75657
andrii <vovchenkoandrii@gmail.com>
2022-09-16 13:22:13 -0700

    added cors

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 
 const app = express();
 //Middlewares
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(bodyParser.json());
 //IMPORT ROUTES
 const realEstateRoutes = require('./routes/real_estate');
